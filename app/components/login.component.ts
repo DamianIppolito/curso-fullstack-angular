a9ea5b606adfba5aba428cf4d69cde102e519952
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-12-22 11:57:55 +0100

    Seccion 12 - 60 - Recibir datos del formulario de login (Two way da...

--- app/components/login.component.ts
+++ app/components/login.component.ts
@@ -5,6 +5,19 @@ import {Component, OnInit} from '@angular/core';
     templateUrl: 'app/view/login.html'
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public titulo : string = "Formulario de login"
+  public user;
+
+  ngOnInit(){
+      this.user = {
+        "email" : "",
+        "password" : "",
+        "gethash" : "false"
+      };
+  }
+
+  onSubmit(){
+    console.log(this.user);
+  }
 }
