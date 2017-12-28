491801bd259f063ea008cbc72098e9dbbcebe17e
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-12-28 09:55:56 +0100

    Seccion 15 - 77 - Crear una clase para los datos

---
+++ app/model/video.ts
@@ -0,0 +1,12 @@
+export class Video{
+  constructor(
+    public id:number,
+    public titulo:string,
+    public description:string,
+    public status:string,
+    public image:string,
+    public videoPath,
+    public createdAt,
+    public updatedAt
+  ){}
+}
